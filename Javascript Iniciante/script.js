104f131c1988245b0cc559270836ada24ec9078a
Leonardo Pereira Vaz <leonardo72011@live.com>
2022-10-05 14:40:30 -0300

    Novos Progetos

---
+++ Javascript Iniciante/script.js
@@ -0,0 +1,7 @@
+function clicou() {
+    let titulo = document.getElementById("#titulo").style("color: red;")
+
+}
+
+let botao = document.querySelector('#botao');
+botao.addEventListener("click", clicou);
