bb77afe84bb20786cb44df48753bfa5eb94c59c7
Leonardo Pereira Vaz <leonardo72011@live.com>
2023-01-16 13:29:15 -0300

    Aula de orientação a objeto

--- Javascript Iniciante/Feito em aula (Alterado constantemente)/script.js
+++ Javascript Iniciante/Feito em aula (Alterado constantemente)/script.js
@@ -1,10 +1,21 @@
-const caucular = () => {
-    const carga = document.getElementById("carga").value;
-    const series = document.getElementById("series").value;
-    const repeticoes = document.getElementById("repeticoes").value;
+class Person {
 
-    const soma = parseFloat(carga) * parseFloat(series) * parseFloat(repeticoes);
+    age = 0;
 
-    document.getElementById("resultado").innerHTML = "Resultado: " + soma;
+    constructor(name, age) {
+        this.name = name;
 
-};
+    };
+}
+
+let p1 = new Person("Anna");
+let p2 = new Person("Laura");
+let p3 = new Person("Jane");
+
+p1.age = 20;
+p2.age = 14;
+p3.age = 32;
+
+console.log(`P1 = ${p1.name} tem ${p1.age} anos.`);
+console.log(`P2 = ${p2.name} tem ${p2.age} anos.`);
+console.log(`P3 = ${p3.name} tem ${p3.age} anos.`)
