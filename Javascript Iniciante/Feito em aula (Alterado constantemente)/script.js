514e68d0f7a16d3a6c2ec5b85948bde3fa9b93f8
Leonardo Pereira Vaz <leonardo72011@live.com>
2023-02-02 14:59:01 -0300

    teste API

--- Javascript Iniciante/Feito em aula (Alterado constantemente)/script.js
+++ Javascript Iniciante/Feito em aula (Alterado constantemente)/script.js
@@ -1,28 +1,12 @@
-class Person {
-
-    age = 0;
-
-    constructor(name) {
-        this.name = name;
-
-    }
-
-  
+function clicou() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => { 
+            return response.json();
+        })
+        .then((json) => {
+            alert(`Titulo do Primero post: ${json[0].title}`)
+        })
 }
 
-function createPerson(name, age){
-    let p = new Person(name);
-    p.age = age;
-    return p;
-}
 
-let p1 = createPerson("Anna",15);
-let p2 = createPerson("Laura",18);
-let p3 = createPerson("Jane",32);
-
-
-
-
-console.log(`P1 = ${p1.name} tem ${p1.age} anos.`);
-console.log(`P2 = ${p2.name} tem ${p2.age} anos.`);
-console.log(`P3 = ${p3.name} tem ${p3.age} anos.`)
+document.querySelector("#botao").addEventListener('click', clicou)
