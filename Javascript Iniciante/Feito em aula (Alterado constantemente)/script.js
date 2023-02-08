eb78e5ce6a1c07d8b56dca1fda987127872a372c
Leonardo Pereira Vaz <leonardo72011@live.com>
2023-02-08 17:30:05 -0300

    Promises com async/await

--- Javascript Iniciante/Feito em aula (Alterado constantemente)/script.js
+++ Javascript Iniciante/Feito em aula (Alterado constantemente)/script.js
@@ -1,13 +1,29 @@
-function clicou() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((response) => { 
-            return response.json();
-        })
-        .then((json) => {
-            alert(`Titulo do Primero post: ${json[0].title}`)
-        })
-}
- 
+async function clicou() {
+    // GET, POST, PUT, DELETE
 
+    let response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    let json = await response.json();
+    alert(`Titulo do Primero post: ${json[0].title}`)
+  
+    alert("CLICOU!")
+};
+
+async function inserir() {
+   let response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: 'Titulo de teste',
+            body: "Corpo de teste",
+            userId: 2
+        })
+    });
+let json = await response.json();
+
+console.log(json);
+}
 
 document.querySelector("#botao").addEventListener('click', clicou)
+document.querySelector("#inserir").addEventListener("click", inserir)
