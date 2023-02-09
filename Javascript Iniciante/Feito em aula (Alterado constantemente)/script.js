0956f0b5859b7e9510582c210aa81e65da653d68
Leonardo Pereira Vaz <leonardo72011@live.com>
2023-02-09 13:25:59 -0300

    Post Fek

--- Javascript Iniciante/Feito em aula (Alterado constantemente)/script.js
+++ Javascript Iniciante/Feito em aula (Alterado constantemente)/script.js
@@ -1,12 +1,41 @@
 function clicou() {
     fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((response) => { 
+        .then((response) => {
             return response.json();
         })
         .then((json) => {
             alert(`Titulo do Primero post: ${json[0].title}`)
         })
+        .catch((error) => {
+            console.log("Deu Problema!");
+            console.log(error);
+        })
+        .finally(() => {
+            alert("Acabou tudo!")
+        })
 }
 
+function inserir() {
+    fetch("https://jsonplaceholder.typicode.com/posts",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify({
+                title: "Titulo de Texto",
+                body: "Corpo de Teste",
+                userId: 2
+            })
+        }
+    )
+    .then((response)=>{
+        return response.json();
+    })
+    .then((json) => {
+        console.log(json);
+    });
+}
 
 document.querySelector("#botao").addEventListener('click', clicou)
+document.querySelector("#inserir").addEventListener('click', inserir)
