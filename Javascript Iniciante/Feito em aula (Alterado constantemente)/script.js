b67316f49e2fa6bea214135108b4d905b18d09b8
Leonardo Pereira Vaz <leonardo72011@live.com>
2023-02-22 12:10:29 -0300

    Funcional: Herança

--- Javascript Iniciante/Feito em aula (Alterado constantemente)/script.js
+++ Javascript Iniciante/Feito em aula (Alterado constantemente)/script.js
@@ -1,24 +1,20 @@
-function createPerson(name, lastName, age, food) {
-    return {
-        name,
-        lastName,
-        age,
-        food,
-        getFull() {
-            return `${this.name} ${this.lastName} tem ${this.age} anos, e gosta de comer ${this.food}.`;
-        },
-        comer() {
-            return `${this.name} come ${this.food}, e fica felis!`;
-        }
-    }
-
-
+const defaultUser = {
+    name: '',
+    email: '',
+    level: 1
 }
 
-let person1 = createPerson('Anna', 'Silva', 12, "Macarão");
-let person2 = createPerson('Julia', 'Leite', 14, "Maçã");
+const user1 = {
+    ...defaultUser,
+    name: 'Anna',
+    email: 'Anna123@gamil.com'
+}
 
-console.log(person1.getFull());
-console.log(person1.comer());
-console.log(person2.getFull());
-console.log(person2.comer());
+const adm1 = {
+    ...defaultUser,
+    name: 'Adão',
+    email: 'AdãoADM@gamil.com',
+    level: 2
+}
+
+console.log(adm1)
