420c192f1c672dd0713c2bf64585c6f561c06b7f
Leonardo Pereira Vaz <leonardo72011@live.com>
2023-03-23 17:58:40 -0300

    Operador Rest

--- Javascript Iniciante/Feito em aula (Alterado constantemente)/script.js
+++ Javascript Iniciante/Feito em aula (Alterado constantemente)/script.js
@@ -1,44 +1,61 @@
 // Descontruindo Objetos
 
-let pessoa = {
-    nome: "Anna",
-    sobrenome: "Leite",
-    // idade: 19,
-    social:{
-        facebook:"AnnaMilk",
-        instagram:{
-            url:"@annaMilk",
-            seguidores:9800
-        }
-    },
+// let pessoa = {
+//     nome: "Anna",
+//     sobrenome: "Leite",
+//     // idade: 19,
+//     social:{
+//         facebook:"AnnaMilk",
+//         instagram:{
+//             url:"@annaMilk",
+//             seguidores:9800
+//         }
+//     },
 
-};
+// };
 
-function nomeCompleto ({nome, sobrenome}){
-    return `${nome} ${sobrenome}`;
+// function nomeCompleto ({nome, sobrenome}){
+//     return `${nome} ${sobrenome}`;
+// }
+
+// let {nome, sobrenome , idade = Math.floor(Math.random() * (20 - 10 + 1) + 10 ), social:{instagram:{url:instagram, seguidores}, facebook} } = pessoa;
+
+// console.log(nomeCompleto(pessoa))
+
+// console.log( `Meu nome é ${nome} ${sobrenome} e eu tenho ${idade}, me siga no Instagram ${instagram} ou no Facebook ${facebook}, eu tenho ${seguidores} seguidores no Instagram.`)
+
+// let numeros = [1,2,3,4];
+
+// let outros = [...numeros, 5,6,7,8];
+
+// let info = {
+//     nome: "Anna",
+//     sobrenome: "Leite",
+//     idade:Math.floor(Math.random * (30 - 20) +20) 
+// }
+
+// let novaInfo ={
+//     ...info,
+//     ciade:"Brasilia",
+//     estado:"DF",
+//     pais:"Brasil"
+// }
+
+// console.log(novaInfo)
+
+
+function adicionar(nome,...novosNomes){
+
+    let conjuto = [
+        ...nome,
+        ...novosNomes
+    ];
+
+    return conjuto
 }
 
-let {nome, sobrenome , idade = Math.floor(Math.random() * (20 - 10 + 1) + 10 ), social:{instagram:{url:instagram, seguidores}, facebook} } = pessoa;
+let nome = ["Anna", "Julia", "Laura"];
 
-console.log(nomeCompleto(pessoa))
+let outros = adicionar(nome, "Paulo", "Lucios","Arnold")
 
-console.log( `Meu nome é ${nome} ${sobrenome} e eu tenho ${idade}, me siga no Instagram ${instagram} ou no Facebook ${facebook}, eu tenho ${seguidores} seguidores no Instagram.`)
-
-let numeros = [1,2,3,4];
-
-let outros = [...numeros, 5,6,7,8];
-
-let info = {
-    nome: "Anna",
-    sobrenome: "Leite",
-    idade:Math.floor(Math.random * (30 - 20) +20) 
-}
-
-let novaInfo ={
-    ...info,
-    ciade:"Brasilia",
-    estado:"DF",
-    pais:"Brasil"
-}
-
-console.log(novaInfo)
+console.log(outros)
