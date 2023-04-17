a9d58521f23120134e690f00b24d3f2582f7cc30
Leonardo Pereira Vaz <leonardo72011@live.com>
2023-04-17 12:05:35 -0300

    Projeto Pizza P6

--- Javascript Intermediário/Projeto Pizza/script.js
+++ Javascript Intermediário/Projeto Pizza/script.js
@@ -1,4 +1,4 @@
-
+let modalQt = 1;
 //Constante que simplifica a execução de querySelector: Função anonima
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
@@ -8,12 +8,32 @@ pizzaJson.map((item, index) => {
     let pizzaItem = c(".models .pizza-item").cloneNode(true);
     //preencher as informações em pizzaitem
 
+
+    pizzaItem.setAttribute("data-key", index);
     pizzaItem.querySelector(".pizza-item--img img").src = item.img;
     pizzaItem.querySelector(".pizza-item--price").innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
     pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description;
     pizzaItem.querySelector("a").addEventListener("click", (e) => {
         e.preventDefault();
+
+        modalQt = 1;
+
+        let key = e.target.closest(".pizza-item").getAttribute("data-key");
+
+        c(".pizzaBig img").src = pizzaJson[key].img;
+        c(".pizzaInfo h1").innerHTML = pizzaJson[key].name;
+        c(".pizzaInfo--desc").innerHTML = pizzaJson[key].description;
+        c(".pizzaInfo--actualPrice").innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        c(".pizzaInfo--size.selected").classList.remove("selected");
+        cs(".pizzaInfo--size").forEach((size, sizeIndex) => {
+            if(sizeIndex == 2){
+                size.classList.add("selected");
+            }
+            size.querySelector("span").innerHTML = pizzaJson[key].sizes[sizeIndex];
+        })
+
+        c(".pizzaInfo--qt").innerHTML = modalQt;
 
         c(".pizzaWindowArea").style.opacity = 0;
         c(".pizzaWindowArea").style.display = "flex";
