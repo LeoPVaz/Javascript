f6234d9df0199e5d33cbe9f326b0ddcc154b5028
Leonardo Pereira Vaz <leonardo72011@live.com>
2023-04-18 17:40:16 -0300

    Projeto Pizza P10

--- Javascript Intermediário/Projeto Pizza/script.js
+++ Javascript Intermediário/Projeto Pizza/script.js
@@ -81,11 +81,8 @@ cs(".pizzaInfo--size").forEach((size, sizeIndex) => {
 });
 
 c(".pizzaInfo--addButton").addEventListener("click", () => {
-
     let size = parseInt(c(".pizzaInfo--size.selected").getAttribute("data-key"));
-
     let identifier = pizzaJson[modalKey].id + "@" + size;
-
     let key = cart.findIndex((item) => item.identifier == identifier);
 
     if (key > -1) {
@@ -99,6 +96,21 @@ c(".pizzaInfo--addButton").addEventListener("click", () => {
     }
 
 
-
+    updateCart()
     closeModal()
 });
+
+function updateCart(){
+    if(cart.length > 0){
+        c("aside").classList.add("show");
+        for(let i in cart){
+
+            let pizzaItem = pizzaJson.find((item)=>item.id = cart[i].id);
+
+            console.log(pizzaItem);
+
+        }
+    }else{
+        c("aside").classList.add("show");
+    }
+}
