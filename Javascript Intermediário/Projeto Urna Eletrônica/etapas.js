61d81a68c1d9c3b13de72e83e9dbef41c2d16928
Leonardo Pereira Vaz <leonardo72011@live.com>
2023-05-17 16:24:06 -0300

    Projeto quade finalisado

---
+++ Javascript Intermediário/Projeto Urna Eletrônica/etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+    {
+        titulo: "Vereador",
+        numero: 5,
+        candidatos: [
+            {
+                numero: "38111",
+                nome: "Anna Milk",
+                partido: "SM",
+                foto: [
+                    { url: "38111.png", legenda: "Veriadora", small: false }
+                ]
+            },
+            {
+                numero: "77222",
+                nome: "Juliha Silva",
+                partido: "GRS",
+                foto: [
+                    { url: "77222.png", legenda: "Veriadora", small: false }
+                ]
+            }
+        ]
+    },
+    {
+        titulo: " Prefeito",
+        numero: 2,
+        candidatos: [
+            {
+                numero: "99",
+                name: "Fernada Café",
+                partido: "WST",
+                vice: "João Fernades",
+                fotos: [{ url: "99.png", legenda: "Prefeita"},
+            {url:"99_2.png",legenda:"Vice-Prefeito", small: true}]
+            },
+            {
+                numero: "84",
+                name: "Olivia Rio Preto",
+                partido: "GFT",
+                vice: "Laura Green",
+                fotos: [{ url: "84.png", legenda: "Prefeita"},
+            {url:"84_2.png",legenda:"Vice-Prefeito", small: true}]
+            }
+        ]
+    }
+];
