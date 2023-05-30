8489844cd619ed61a96a78e866b3e2bf287c51b1
Leonardo Pereira Vaz <leonardo72011@live.com>
2023-05-30 15:50:34 -0300

    JS Porjeto Violao p1

---
+++ Javascript Intermediário/Projeto Violão/script.js
@@ -0,0 +1,8 @@
+let corda = document.querySelector(".corda::before");
+let nota = document.querySelector(".nota-corda");
+
+
+corda.addEventListener("click", () => {
+    corda.classList.add(".vibracao");
+    console.log("ok");
+})
