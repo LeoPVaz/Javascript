52eae1541e1eff04b70a1944397f6568bb0fb8e8
Leonardo Pereira Vaz <leonardo72011@live.com>
2023-06-02 16:17:13 -0300

    Projeto Violão P2

--- Javascript Intermediário/Projeto Violão/script.js
+++ Javascript Intermediário/Projeto Violão/script.js
@@ -1,55 +1,34 @@
-let corda = document.querySelector(".corda");
-let notas = document.querySelectorAll(".nota-corda");
+// let corda = document.querySelector(".corda");
+// let notas = document.querySelectorAll(".nota-corda");
 
-notas.forEach((nota, index) => {
+
+let cordas = document.querySelectorAll(".corda");
+
+cordas.forEach((corda) => {
+  let notas = corda.querySelectorAll(".nota-corda");
+
+  notas.forEach((nota, _index) => {
     nota.addEventListener("click", () => {
-      for (let i = index + 1; i < notas.length; i++) {
-        notas[i].classList.toggle("vibracao");
+      let clickedNoteFound = false;
+
+      for (let i = 0; i < notas.length; i++) {
+        if (clickedNoteFound) {
+          notas[i].classList.add("vibracao");
+        }
+
+        if (nota === notas[i]) {
+          clickedNoteFound = true;
+        }
       }
+
+      nota.classList.remove("vibracao");
+
+      setTimeout(() => {
+        notas.forEach((nota) => {
+          nota.classList.remove("vibracao");
+        });
+      }, 300);
+      
     });
   });
-
-// nota.forEach((nota, index) =>{
-//     nota.addEventListener("click", () => {
-//         if(nota.classList.add("vibracao")){
-//             for(let i = index + 1; i < nota.clientHeight; i ++){
-//         nota[i].classList.remove("vibracao");
-//         }
-//         }else{
-//              for( let i = index +1; i < nota.length; i ++){
-//         nota[i].classList.add("vibracao");
-//         }
-   
-//         }
-    
-
-   
-    
-//     })
-// });
-
-
-// // Adiciona o evento de clique a cada nota
-// notas.forEach((nota, index) => {
-//     nota.addEventListener('click', () => {
-//       // Remove a classe "vibrating" de todas as notas após a clicada
-//       for (let i = index + 1; i < notas.length; i++) {
-//         notas[i].classList.remove('vibrating');
-//       }
-  
-//       // Adiciona a classe "vibrating" às notas após a clicada
-//       for (let i = index + 1; i < notas.length; i++) {
-//         notas[i].classList.add('vibrating');
-//       }
-//     });
-//   });
-
-
-// let corda = document.querySelector(".corda");
-// let notas = corda.querySelectorAll(".nota-corda");
-
-// notas.forEach((nota) => {
-//   nota.addEventListener("click", () => {
-//     nota.classList.toggle("vibracao");
-//   });
-// });
+});
