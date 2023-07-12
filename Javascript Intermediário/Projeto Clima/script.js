40a7075da4bf0447a5d63a480f6629e5a5518580
Leonardo Pereira Vaz <leonardo72011@live.com>
2023-07-12 16:58:52 -0300

    Modificações no Projeto Clima

--- Javascript Intermediário/Projeto Clima/script.js
+++ Javascript Intermediário/Projeto Clima/script.js
@@ -8,7 +8,6 @@ document.querySelector(".busca").addEventListener("submit", async (event) => {
     if (input !== "") {
         claerInfo();
         showWarning("Carregando...")
-
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=95d0315c872e0cbb81184f16031efd30&units=metric&lang=pt_br`;
 
         let resultado = await fetch(url);
@@ -21,6 +20,9 @@ document.querySelector(".busca").addEventListener("submit", async (event) => {
                 name: json.name,
                 country: json.sys.country,
                 temp: json.main.temp,
+                max: json.main.temp_max,
+                min: json.main.temp_min,
+                sensacao:json.main.feels_like,
                 tempIcon: json.weather[0].icon,
                 windSpeed: json.wind.speed,
                 windAngle: json.wind.deg
@@ -34,11 +36,22 @@ document.querySelector(".busca").addEventListener("submit", async (event) => {
     }
 });
 
+function tempBk(json){
+    if(30 == json.temp){
+        document.querySelector("body").style.backgroundColor = "red";
+    }
+}
+
 function showInfo(json) {
     showWarning("");
 
     document.querySelector(".titulo").innerHTML = `${json.name}, ${json.country}`;
     document.querySelector(".tempInfo").innerHTML = `${json.temp} <sup>°C</sup>`;
+    document.querySelector(".senInfo").innerHTML = `${json.sensacao}<sup>°C</sup>`;
+    document.querySelector(".minInfo").innerHTML = `${json.min} <sup>°C</sup>`;
+    document.querySelector(".maxInfo").innerHTML = `${json.max} <sup>°C</sup>`;
+
+
     document.querySelector(".ventoInfo").innerHTML = `${json.windSpeed} <span>km/h</span>`;
 
     document.querySelector(".temp img").setAttribute("src", `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
