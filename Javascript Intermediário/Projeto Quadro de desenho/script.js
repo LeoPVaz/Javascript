46524fafdb74e70236d9802532b32fb6ad0715d4
Leonardo Pereira Vaz <leonardo72011@live.com>
2023-07-18 16:58:40 -0300

    Finalização do Projeto Quadro de Desenho

--- Javascript Intermediário/Projeto Quadro de desenho/script.js
+++ Javascript Intermediário/Projeto Quadro de desenho/script.js
@@ -1,5 +1,8 @@
 // Inicial Data
 let currentColor = "black";
+let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
 
 let screen = document.querySelector("#tela");
 let ctx = screen.getContext("2d");
@@ -7,17 +10,55 @@ let ctx = screen.getContext("2d");
 // Events
 document.querySelectorAll(".colorArea .color").forEach(item => {
     item.addEventListener("click", colorClickEvent);
-    
-});
 
-screen.addEventListener("mausedown", mouseDownEvent);
+});
+screen.addEventListener("mousedown", mouseDownEvent);
 screen.addEventListener("mousemove", mouseMoveEvent);
+screen.addEventListener("mouseup", mauseUpEvent);
+document.querySelector(".clear").addEventListener("click", claerScreen);
 
 // Functions
-function colorClickEvent(e){
+function colorClickEvent(e) {
     let color = e.target.getAttribute("data-color");
     currentColor = color;
-
     document.querySelector(".color.active").classList.remove("active");
     e.target.classList.add("active");
+}
+
+function mouseDownEvent(e) {
+    canDraw = true;
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
+}
+
+function mouseMoveEvent(e) {
+    if (canDraw) {
+        draw(e.pageX, e.pageY);
+    }
+}
+
+function mauseUpEvent() {
+    canDraw = false;
+}
+
+function draw(x, y) {
+    let pointX = x - screen.offsetLeft;
+    let pointy = y - screen.offsetTop;
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.linejoin = "round";
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointy);
+    ctx.closePath();
+    ctx.strokeStyle = currentColor;
+    ctx.stroke();
+
+    mouseX = pointX;
+    mouseY = pointy;
+}
+
+function claerScreen() {
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
